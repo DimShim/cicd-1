9ecf0f42d58a1d0189ad3e809070869f23363559
DimShim <en_di@mail.ru>
2021-10-20 11:44:32 +0300

    new

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    page = '<html><body><h1>'
+    page += 'Hello World 1'
+    page += '</h1></body></html>'
+    return page
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
